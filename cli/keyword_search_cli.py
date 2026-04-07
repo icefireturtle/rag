d6f801e50258106070f4c74ce5833df09a61d3a7
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2026-04-06 22:20:19 -0500

    added keyword search to review data using json.load, search for the keyword through the keys, append to results, truncate to 5 max, and print results in ascending order based on data source and numbered

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -8,13 +9,25 @@ def main() -> None:
     search_parser.add_argument("query", type=str, help="Search query")
 
     args = parser.parse_args()
+    results = []
 
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
-            pass
+            with open('data/movies.json', 'r') as file:
+                data = json.load(file)
+                movies = data['movies']
+                for movie in movies:
+                    if len(results) == 5: 
+                        break
+                    if args.query in movie['title']:
+                        results.append(movie)
         case _:
             parser.print_help()
+
+    if results: 
+        for i in range(len(results)):
+            print(f"{i+1}. {results[i]['title']}")
 
 if __name__ == "__main__":
     main()
